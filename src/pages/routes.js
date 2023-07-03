8091dec34bad957b5b18e6c186f4e43900b49ee7
Anthony Martin <anthonymartin20@gmail.com>
2023-07-03 14:12:51 -0400

    Add register form

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,6 +1,7 @@
 import {createRouter} from 'vue-router'
 import HomePage from './home/Home.vue';
 import LoginPage from './session/Login.vue';
+import RegisterPage from './session/Register.vue';
 
 const routes = [
   {
@@ -10,6 +11,10 @@ const routes = [
   {
     path: '/login',
     component: LoginPage
+  },
+  {
+    path: '/register',
+    component: RegisterPage
   }
 ]
 
---
+++ src/pages/session/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <form>
+      <div class="form-line">
+        <h1>Registro de usuario</h1>
+      </div>
+      <div class="form-line">
+        <label for="name">Nombre</label>
+        <input type="text" id="name" />
+      </div>
+      <div class="form-line">
+        <label for="email">Email</label>
+        <input type="email" id="email" />
+      </div>
+      <div class="form-line">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" />
+      </div>
+      <div class="form-line">
+        <label for="password">Confirmar contraseña</label>
+        <input type="password" id="password" />
+      </div>
+      <div class="form-line">
+        <button type="submit" class="btn primary">Registrarse</button>
+      </div>
+      <div class="form-line">
+        <router-link to="/session/register">Iniciar sesión</router-link>
+      </div>
+    </form>
+  </main>  
+</template>
+
+<style scoped>
+
+</style>
