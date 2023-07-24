896bf2088933f9efda60ed1cbe4766b5d074c489
Anthony Martin <anthonymartin20@gmail.com>
2023-07-24 08:42:07 -0400

    Create BaseSearch component

---
+++ src/components/base/BaseSearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="big-search">
+    <input
+      type="text"
+      placeholder="Buscar artículo"
+      id="search"
+      v-model="filter"
+      @keyup.enter="$emit('search', filter)"
+    />
+    <label for="search">
+      <font-awesome-icon icon="magnifying-glass" />
+    </label>
+</section>
+</template>
+
+<script>
+  export default {
+    emits: ['search'],
+
+    data() {
+      return {
+        filter: '',
+      }
+    }
+  }
+</script>
