9cb12b9de46bb68f045d0833aba266c69294b374
Anthony Martin <anthonymartin20@gmail.com>
2023-10-07 15:03:23 -0400

    Reusing forms

---
+++ src/components/forms/ItemsForm.vue
@@ -0,0 +1,124 @@
+<template>
+  <section class="r-form-wrapper">
+    <form class="r-form">
+      <h2 class="r-title">Nuevo artículo</h2>
+      <div>
+        <div class="card">
+          <div class="field">
+              <label for="code">Código</label>
+              <input-text id="code" class="w-full" :class="invalidClass('code')"
+                @input="$emit('update:code', $event.target.value)"
+                :value="code"
+              />
+              <Message severity="error" v-if="invalidClass('code')">El código debe ser de al menos 3 caracteres</Message>
+          </div>
+          <div class="field">
+              <label for="name">Nombre</label>
+              <input-text id="name" class="w-full" :class="invalidClass('name')"
+                @input="$emit('update:name', $event.target.value)"
+                :value="name"
+              />
+              <Message severity="error" v-if="invalidClass('name')">El nombre debe ser de al menos 5 caracteres</Message>
+          </div>
+          <div class="field">
+            <label for="model">Modelo</label>
+            <input-text id="model" class="w-full" :class="invalidClass('model')"
+              @input="$emit('update:model', $event.target.value)"
+              :value="model"
+            />
+            <Message severity="error" v-if="invalidClass('model')">El modelo debe ser de al menos 5 caracteres</Message>
+          </div>
+          <div class="flex gap-3">
+            <div class="field w-6">
+              <label for="price">Precio ({{baseCurrency?.code}})</label>
+              <input-number id="price" :class="invalidClass('price')" mode="currency" :currency="baseCurrency?.code"
+                @input="$emit('update:price', $event.value)"
+                :modelValue="price"
+              />
+              <Message severity="error" v-if="invalidClass('price')">El precio debe ser mayor que 0</Message>
+            </div>
+            <div class="field w-6">
+              <label for="stock">Disponible</label>
+              <input-number id="stock"  :class="invalidClass('stock')" :suffix="unit?.code"
+                @input="$emit('update:stock', $event.value)"
+                :modelValue="stock"
+              />
+              <Message severity="error" v-if="invalidClass('stock')">El stock debe ser mayor que 0</Message>
+            </div>
+          </div>
+          <div class="field">
+            <label for="unit">Unidad</label>
+            <Dropdown
+              @change="$emit('update:unit', $event.value)"
+              :modelValue="unit"
+              :options="units"
+              optionLabel="name"
+              filter
+              :style="{ width: '100%' }"
+              :class="invalidClass('unit')"
+            />  
+          </div>
+          <Message severity="error" v-if="invalidClass('unit')">Escoge la unidad de la lista</Message>
+          <div class="field">
+            <Button class="w-full justify-content-center" @click="create">Crear</Button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+import ValidatorService from '../../services/ValidatorService';
+export default {
+  name: 'ItemsForm',
+  props: ['code', 'name', 'model', 'price', 'stock', 'unit', 'baseCurrency', 'units'],
+  emits: ['create', 'update:code', 'update:name', 'update:model', 'update:price', 'update:stock', 'update:unit'],
+  data() {
+    return {
+        errors: '',
+    }
+  },
+
+  methods: {
+    create(){
+      this.errors = []
+
+      if(!this.validateCode) this.errors.push('code');
+      if(!this.validateName) this.errors.push('name');
+      if(!this.validateModel) this.errors.push('model');
+      if(!this.validatePrice) this.errors.push('price');
+      if(!this.validateStock) this.errors.push('stock');
+      if(!this.validateUnit) this.errors.push('unit');
+
+      this.$emit('create', this.errors);
+    },
+    invalidClass(field){
+      if (this.errors.includes(field)) {
+        return 'p-invalid';
+      }
+    },
+  },
+
+  computed: {
+    validateCode(){
+      return ValidatorService.validateCode(this.code);
+    },
+    validateName(){
+      return ValidatorService.validateString(this.name);
+    },
+    validateModel(){
+      return ValidatorService.validateString(this.model);
+    },
+    validatePrice(){
+      return ValidatorService.validateAmount(this.price);
+    },
+    validateStock(){
+      return ValidatorService.validateAmount(this.stock);
+    },
+    validateUnit(){
+      return ValidatorService.validateItemList(this.units, this.unit, 'name');
+    },
+  },
+}
+</script>
--- src/pages/items/ItemsNew.vue
+++ src/pages/items/ItemsNew.vue
@@ -1,70 +1,30 @@
 <template>
-  <section class="r-form-wrapper">
-    <form class="r-form">
-      <h2 class="r-title">Nuevo artículo</h2>
-      <div>
-        <div class="card">
-          <div class="field">
-              <label for="code">Código</label>
-              <input-text id="code" class="w-full" v-model="item.code" :class="invalidClass('code')" />
-          </div>
-          <Message severity="error" v-if="invalidClass('code')">El código debe ser de al menos 3 caracteres</Message>
-          <div class="field">
-              <label for="name">Nombre</label>
-              <input-text id="name" class="w-full" v-model="item.name" :class="invalidClass('name')" />
-              <Message severity="error" v-if="invalidClass('name')">El nombre debe ser de al menos 5 caracteres</Message>
-          </div>
-          <div class="field">
-            <label for="model">Modelo</label>
-            <input-text id="model" class="w-full" v-model="item.model" :class="invalidClass('model')" />
-            <Message severity="error" v-if="invalidClass('model')">El modelo debe ser de al menos 5 caracteres</Message>
-          </div>
-          <div class="flex gap-3">
-            <div class="field w-6">
-              <label for="price">Precio ({{baseCurrency?.code}})</label>
-              <input-number id="price" v-model="item.price"  :class="invalidClass('price')" />
-              <Message severity="error" v-if="invalidClass('price')">El precio debe ser mayor que 0</Message>
-            </div>
-            <div class="field w-6">
-              <label for="stock">Disponible</label>
-              <input-number id="stock" v-model="item.stock"  :class="invalidClass('stock')" />
-              <Message severity="error" v-if="invalidClass('stock')">El stock debe ser mayor que 0</Message>
-            </div>
-          </div>
-          <div class="field">
-            <label for="unit">Unidad</label>
-            <Dropdown
-              v-model="item.unit"
-              :options="units"
-              optionLabel="name"
-              filter
-              :style="{ width: '100%' }"
-              :class="invalidClass('unit')"
-            />  
-          </div>
-          <Message severity="error" v-if="invalidClass('unit')">Escoge la unidad de la lista</Message>
-          <div class="field">
-            <Button class="w-full justify-content-center" @click="createNewItem">Crear</Button>
-          </div>
-        </div>
-      </div>
-    </form>
-  </section>
+  <ItemsForm
+    :baseCurrency="baseCurrency"
+    :units="units"
+
+    v-model:code="item.code"
+    v-model:name="item.name"
+    v-model:model="item.model"
+    v-model:price="item.price"
+    v-model:stock="item.stock"
+    v-model:unit="item.unit"
+  
+    @create="createNewItem"
+  />
 </template>
 
 <script>
-/*
-To do:
-  validate fields
-  create item
-*/
 import { useCorporation } from '../../store/corporation';
 import { useItem } from '../../store/items';
 import { mapState, mapActions } from 'pinia';
-import validatorService from '../../services/ValidatorService';
+import ItemsForm from '../../components/forms/ItemsForm.vue';
 
 export default {
   name: 'ItemsNew',
+  components: {
+    ItemsForm,
+  },
   data() {
     return {
       item: {
@@ -75,7 +35,6 @@ export default {
         stock: 0,
         unit: '',
       },
-      invalids: [],
     }
   },
 
@@ -87,18 +46,8 @@ export default {
   methods: {
     ... mapActions(useCorporation, ['getUnits', 'getBaseCurrency']),
     ... mapActions(useItem, ['createItem', 'getItems']),
-    async createNewItem(){
-      console.log('dandole')
-      this.invalids = [];
-
-      if(!this.validateCode) this.invalids.push('code');
-      if(!this.validateName) this.invalids.push('name');
-      if(!this.validateModel) this.invalids.push('model');
-      if(!this.validatePrice) this.invalids.push('price');
-      if(!this.validateStock) this.invalids.push('stock');
-      if(!this.validateUnit) this.invalids.push('unit');
-
-      if(this.invalids.length > 0) {
+    async createNewItem(errors){
+      if(errors.length > 0) {
         return;
       }
       let result = null;
@@ -121,33 +70,10 @@ export default {
         this.$toast.add({ severity: 'error', summary: 'Error', detail: 'No se pudo crear el artículo', life: 3000 });
       }
     },
-    invalidClass(field){
-      if (this.invalids.includes(field)) {
-        return 'p-invalid';
-      }
-    },
   },
 
   computed: {
     ... mapState(useCorporation, ['units', 'baseCurrency']),
-    validateCode(){
-      return validatorService.validateCode(this.item.code);
-    },
-    validateName(){
-      return validatorService.validateString(this.item.name);
-    },
-    validateModel(){
-      return validatorService.validateString(this.item.model);
-    },
-    validatePrice(){
-      return validatorService.validateAmount(this.item.price);
-    },
-    validateStock(){
-      return validatorService.validateAmount(this.item.stock);
-    },
-    validateUnit(){
-      return validatorService.validateItemList(this.units, this.item.unit, 'name');
-    },
   },
 }
 </script>
