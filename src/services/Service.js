fb46c175674106e5b44d51d18691c6c10e2039e5
Anthony Martin <anthonymartin20@gmail.com>
2024-02-01 17:47:21 -0400

    Using public IP

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-// const baseURL = 'http://179.0.104.124:3000/api/v1/';
+const baseURL = 'http://179.0.104.124:3000/api/v1/';
 // const baseURL = 'http://192.168.0.237:3000/api/v1/';
-const baseURL = 'http://localhost:3000/api/v1/';
+// const baseURL = 'http://localhost:3000/api/v1/';
 
 const Service = axios.create({ baseURL });
 
