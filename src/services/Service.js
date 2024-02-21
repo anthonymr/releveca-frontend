5ed66bd92a8d4774c3dd4bc7e143f8011c95fea3
Anthony Martin <anthonymartin20@gmail.com>
2024-02-21 11:23:30 -0400

    Update Service.js

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const backEndURL = 'http://localhost:3000/';
+// const backEndURL = 'http://localhost:3000/';
 // const backEndURL = 'http://179.0.104.124:3000/';
-// const backEndURL = 'http://192.168.0.237:3000/';
+const backEndURL = 'http://192.168.0.237:3000/';
 
 const baseURL = `${backEndURL}api/v1/`;
 
