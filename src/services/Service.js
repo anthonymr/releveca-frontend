7cd41527a2fe95d9c295b41b8cb39f5b4c071967
Anthony Martin <anthonymartin20@gmail.com>
2024-03-04 08:46:42 -0400

    No more differents backend url

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
+const port =  '3000';
+
+let be = window.location.origin;
+be = be.replace(/:\d+$/, '');
+const backEndURL = `${be}:${port}/`;
+
 // const backEndURL = 'http://localhost:3000/';
 // const backEndURL = 'http://179.0.104.124:3000/';
-const backEndURL = 'http://192.168.0.237:3000/';
+// const backEndURL = 'http://192.168.0.237:3000/';
 
 const baseURL = `${backEndURL}api/v1/`;
 
